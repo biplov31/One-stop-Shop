3fbaee98a89c79fe793369e8236e9af3f81e1367
biplov31 <biplovthapa31@gmail.com>
2023-07-21 19:42:49 +0545

    Add search functionality

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
+
 export const Search = () => {
+  const [searchTerm, setSearchTerm]  = useState('');
+
+  const fetchProducts = async () => {
+    const res = await fetch('https://fakestoreapi.com/products?limit=30');
+    return res.json();
+  }
+
+  const { data: products } = useQuery(['products'], fetchProducts)
+  
   return (
-    <h1>Search Page</h1>
+    <main>
+      <input 
+        type="text" 
+        className="searchbar"
+        placeholder="Search for products" 
+        onChange={(e) => setSearchTerm(e.target.value)} 
+      />
+      <div className="search-results">
+        {products && products.filter((product) => {
+          if (searchTerm == '') {
+            return
+          } else if (product.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return product
+          }
+        }).map((product) => {
+          return (
+            <Link key={product.id} to={`/products/${product.id}`} >
+              <div className="search-result">
+                <img src={product.image} alt={product.title} />
+                <span>{product.title}</span>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </main>
   )
 }
